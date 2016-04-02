fa40a45984de0bd9029d64d677762ae6e3a9ef40
Hudson Souza <hudsonss@gmail.com>
2016-04-02 10:43:55 -0300

    M7Ex1 - novo

--- onlinemarket.work/module/Market/config/module.config.php
+++ onlinemarket.work/module/Market/config/module.config.php
@@ -1,80 +1,169 @@
 <?php
 
-return array(
+/*
+### CONTROLE DE ROTAS - M7Ex1
+routes(
+    home()
+        market(
+            view(
+                main()
+                item()
+            )
+            post()
+        )
+)
+*/
+
+ return array(
     
-    // CONTROLE DAS ROTAS - URL
-    'router' => array(
-        'routes' => array(
-            'market' => array(  // NOME DA URL
-                'type' => 'Literal',  // CAMINHO FIXO  / NÁO É DINAMICO
+     // CONTROLE DAS ROTAS - URL
+     
+    'router' => array(  // ROUTER
+          
+        'routes' => array(  // ROUTES
+     
+            
+            // M7Ex1 - PARTE A
+            'home' => array(  // ROTA 0
+                'type' => 'Literal',
                 'options' => array(
-                    'route'    => '/market',  // CAMINHO DA URL
+                    'route' => '/',
                     'defaults' => array(
-                        //'controller' => 'Market\Controller\Index',
                         'controller' => 'market-index-controller',
-                        'action'     => 'index',
+                        'action' => 'index',
                     ),
                 ),
-                
-                // DEIXA A ROTA DINAMICA
+            ), // fim ROTA 0
+            
+            
+             // M7Ex1 - PARTE B
+            'market' => array( // ROTA 1 - PAI
+                'type' => 'Literal',
+                'options' => array(
+                    'route' => '/market',
+                    'defaults' => array(
+                        'controller' => 'market-index-controller',
+                        'action' => 'index',
+                    ),
+                ),
+             
+           
                 'may_terminate' => true,
-                'child_routes' => array(
-                    'default' => array(
-                        'type'    => 'Segment',
-                        'options' => array(
-                            'route'    => '/[:controller[/:action]]',
-                            'constraints' => array(
-                                'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
-                            ),
-                            'defaults' => array(
+                'child_routes' => array(  // FILHA 1
+                    
+                // VIEW
+                'view' => array(  // FILHA 1.1
+                    'type' => 'Literal',
+                    'options' => array(
+                        'route' => '/view',
+                        'defaults' => array(
+                            'controller' => 'market-view-controller',
+                            'action' => 'index',
+                        ),
+                    ),                    
+                  
+                    'may_terminate' => true,
+                    'child_routes' => array(  // FILHA 2
+                    
+                        // MAIN
+                        'main' => array(
+                            'type'    => 'Segment',
+                            'options' => array(
+                                //'route'    => '/[:controller[/:action]]',
+                                'route'    => '/main[/:category]',
+                                //  'constraints' => array(
+                                //  'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                                //  'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
+                                //  ),
+                                'defaults' => array(
+                                    'action' => 'index',
+                                ),
                             ),
                         ),
-                    ),
-                ),
-  
-            ),
-        ),
-    ),
+
+                        
+                        // ITEM
+                        'item' => array(
+                            'type'    => 'Segment',
+                            'options' => array(
+                                'route'    => '/item[/:itemId]',
+                                'defaults' => array(
+                                    'action' => 'item',
+                                ),
+                                'constraints' => array(
+                                    'itemId' => '[0-9]*',
+                                ),                            
+                            ),
+                        ),                         
+   
+                    ),  // fim FILHA 2
+                    
+                ),  // fim FILHA 1.1    
+                    
+                    
+                // POST
+                // M7Ex1 - PARTE C
+                'post' => array(  // FILHA 1.2
+                     'type' => 'Literal',
+                     'options' => array(
+                         'route' => '/post',
+                         'defaults' => array(
+                             'controller' => 'market-post-controller',
+                             'action' => 'index',
+                         ),
+                     ),
+                 ),  // fim FILHA 1.2                
     
+                ), // fim FILHA 1
+                 
+            ),  // fim ROTA 1 - PAI      
     
-    // SM - REGISTRO DO SERVICO
-    'controllers' => array(
-        'invokables' => array(  // TIPO DO SERVICO
-            // NOME DO SERVICO        /  CAMINHO DA CLASSE
-            //'Market\Controller\Index' => 'Market\Controller\IndexController',  // SERVICO / URL DA CLASSE
-            
-            // MUDAR O NOME DO REGISTRO DO CONTROLLER PARA SER ACESSADO PELA URL EXPECÍFICA
-            'market-index-controller' => 'Market\Controller\IndexController',  // SERVICO / URL DA CLASSE
-            // REGISTRO DA 'viewController' M6Ex1 - parte A
-            'market-view-controller' => 'Market\Controller\ViewController',
-            ),
-        
-        // REGISTRO DA 'viewController' M6Ex1 - parte B
-        'factories' => array(
-           'market-post-controller' => 'Market\Factory\PostControllerFactory',
-        ),
-        
-    ),
+        ), // fim ROUTES
+     
+    ),  // fim ROTER
     
-    
-    // VIEW MANAGER
-        'view_manager' => array(
-//        'display_not_found_reason' => true,
-//        'display_exceptions'       => true,
-//        'doctype'                  => 'HTML5',
-//        'not_found_template'       => 'error/404',
-//        'exception_template'       => 'error/index',
-//        'template_map' => array(
-//            'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
-//            'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
-//            'error/404'               => __DIR__ . '/../view/error/404.phtml',
-//            'error/index'             => __DIR__ . '/../view/error/index.phtml',
-//        ),
-        'template_path_stack' => array(
-            __DIR__ . '/../view',
-        ),
-    ),
-    
-    
-);
+     
+     // SM - REGISTRO DO SERVICO
+     'controllers' => array(
+         'invokables' => array(  // TIPO DO SERVICO
+             // NOME DO SERVICO        /  CAMINHO DA CLASSE
+             //'Market\Controller\Index' => 'Market\Controller\IndexController',  // SERVICO / URL DA CLASSE
+             
+             // MUDAR O NOME DO REGISTRO DO CONTROLLER PARA SER ACESSADO PELA URL EXPECÍFICA
+             'market-index-controller' => 'Market\Controller\IndexController',  // SERVICO / URL DA CLASSE
+             // REGISTRO DA 'viewController' M6Ex1 - parte A
+             'market-view-controller' => 'Market\Controller\ViewController',
+             ),
+         
+         // REGISTRO DA 'viewController' M6Ex1 - parte B
+         'factories' => array(
+            'market-post-controller' => 'Market\Factory\PostControllerFactory',
+         ),
+         // M6Ex1  - PARTE D - CRIAR APELIDO PARA URL
+         'aliases' => array(
+            'alt' => 'market-view-controller', 
+         ),
+         
+     ),
+     
+     
+     // VIEW MANAGER
+         'view_manager' => array(
+ //        'display_not_found_reason' => true,
+ //        'display_exceptions'       => true,
+ //        'doctype'                  => 'HTML5',
+ //        'not_found_template'       => 'error/404',
+ //        'exception_template'       => 'error/index',
+ //        'template_map' => array(
+ //            'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
+ //            'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
+ //            'error/404'               => __DIR__ . '/../view/error/404.phtml',
+ //            'error/index'             => __DIR__ . '/../view/error/index.phtml',
+ //        ),
+         'template_path_stack' => array(
+             __DIR__ . '/../view',
+         ),
+     ),
+     
+     
+ );
--- onlinemarket.work/module/Market/src/Market/Controller/ViewController.php
+++ onlinemarket.work/module/Market/src/Market/Controller/ViewController.php
@@ -1,10 +1,40 @@
 <?php
-
-namespace Market\Controller;
-
-class ViewController extends \Zend\Mvc\Controller\AbstractActionController
-{
-    public function indexAction() {
-        return new \Zend\View\Model\ViewModel(array('category'=>'category postings'));
-    }
-}
+ 
+ namespace Market\Controller;
+ 
+ class ViewController extends \Zend\Mvc\Controller\AbstractActionController
+ {
+     public function indexAction() {
+         // PARTE B
+         //return new \Zend\View\Model\ViewModel(array('category'=>'category postings'));
+         
+         // PARTE C
+         //$category  = $this->params()->fromQuery("category");
+         
+         //M7Ex1 - Parte D
+         $category  = $this->params()->fromRoute("category");
+         
+         return new \Zend\View\Model\ViewModel(array('category'=>$category));  
+     }
+     
+     public function itemAction()
+     {
+         // M6Ex1 - Parte C
+         //$itemId = $this->params()->fromQuery('itemId');
+         
+         //M7Ex1 - Parte D
+         $itemId = $this->params()->fromRoute('itemId');
+         
+         
+         
+         // REDIRECIONA PARA PAGINA 'home' CASO itemId ESTAJA EM BRANCO
+         if(!$itemId){
+             $this->flashMessenger()->addMessage("Item not found");
+             //return $this->redirect()->toRoute('home');
+             return $this->redirect()->toRoute('market');
+         }
+         
+         return new \Zend\View\Model\ViewModel(array('itemId'=>$itemId));
+     }
+            
+ }
